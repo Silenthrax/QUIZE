42693b39a14f5fa7fb508c6fdfaf93afeb376ee0
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-15 15:16:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN : "",
-  OWNER_ID : [],
-  MONGO_DB : ""
+  BOT_TOKEN : "7832596357:AAFqq0NUHW9fQKi_XjmrLPS3-FJICXb3-Mg",
+  OWNER_ID : [6107581019],
+ // MONGO_DB : "" //
 };
