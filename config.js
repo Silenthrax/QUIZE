b47e72ef894465081828194f411947e287da149b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-17 08:01:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const config = {
+const configs = {
   BOT_TOKEN: "7832596357:AAFqq0NUHW9fQKi_XjmrLPS3-FJICXb3-Mg",
   OWNER_ID: [6107581019]
 };
 
-module.exports = config;
+module.exports = configs;
