55bdcaec28fd844250edbbda3595747b47608024
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-17 22:13:20 +0530

    Create callback.js

---
+++ QuizBot/plugins/callback.js
@@ -0,0 +1,17 @@
+const bot = require("../index");
+const AddUsersQuiz = require("/QuizBot/plugins/addquiz")
+
+
+
+bot.on('callback_query', (ctx) => {
+    const callbackData = ctx.callbackQuery.data;
+    if (callbackData === 'delete_me') {
+        return ctx.answerCallbackQuery('Deleted...');
+    } else if (callbackData === 'add_more') {
+        return ctx.answerCallbackQuery('Add More Quizes...');
+    } 
+});
+
+
+
+
