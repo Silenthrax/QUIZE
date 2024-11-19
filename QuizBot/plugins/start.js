f4d23e4d07fdcaafb61cf35681b74e7b62a8508d
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-19 10:08:09 +0530

    Update start.js

--- QuizBot/plugins/start.js
+++ QuizBot/plugins/start.js
@@ -3,7 +3,7 @@ const { Markup } = require('telegraf');
 
 bot.command("start", (ctx) => {
     let name = ctx.from.first_name;
-    ctx.reply(`Hello, ${name},\n\nI am your new Quiz Bot, and I’m built using JavaScript.`,
+    ctx.reply(`Hello, ${name},\n\nWelcome to QuizBot! I'm here to help you create and organize quizzes effortlessly. Just save your questions, and let's turn them into interactive quizzes!`,
         Markup.inlineKeyboard([
             [Markup.button.url("Source", "https://github.com/Sumit0045/QuizBot")]
         ]));
