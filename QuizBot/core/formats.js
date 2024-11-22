fe1de1708a03d7bcfaf17dd49bad333ccd6ce2f4
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-22 20:10:03 +0530

    Create formats.js

---
+++ QuizBot/core/formats.js
@@ -0,0 +1,32 @@
+
+// ---------------- Start Text ---------------- //
+
+const START_TEXT = {
+"English": "",
+"Hindi" : "",
+"Chinese": "",
+"Russian": ""
+}
+
+// ---------------- Tools Text ---------------- //
+
+const TOOLS_TEXT = {
+"English": "",
+"Hindi" : "",
+"Chinese": "",
+"Russian": ""
+}
+
+// ---------------- About Text ---------------- //
+
+const ABOUT_TEXT = {
+"English": "",
+"Hindi" : "",
+"Chinese": "",
+"Russian": ""
+}
+
+
+// ---------------- Questions ---------------- //
+
+ 
