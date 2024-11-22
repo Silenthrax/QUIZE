f43d54c64d985c7dab1668b6900164161c1fe985
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-22 20:21:02 +0530

    Update formats.js

--- QuizBot/core/formats.js
+++ QuizBot/core/formats.js
@@ -2,31 +2,42 @@
 // ---------------- Start Text ---------------- //
 
 const START_TEXT = {
-"English": `Hello, ${name},\n\nWelcome to QuizBot! I'm here to help you create and organize quizzes effortlessly. Just save your questions, and let's turn them into interactive quizzes!`,
-"Hindi" : "नमस्ते, ${name},\n\nQuizBot में आपका स्वागत है! मैं आपकी मदद करने के लिए यहां हूं ताकि आप आसानी से क्विज बना और आयोजित कर सकें। बस अपने प्रश्नों को सहेजें, और आइए उन्हें इंटरएक्टिव क्विज़ में बदलें!",
-"Chinese": "你好, ${name},\n\n欢迎来到 QuizBot！我在这里帮助您轻松创建和组织测验。只需保存您的问题，让我们将它们变成互动测验！",
-"Russian": "Здравствуйте, ${name},\n\nДобро пожаловать в QuizBot! Я здесь, чтобы помочь вам легко создавать и организовывать викторины. Просто сохраните свои вопросы, и давайте превратим их в интерактивные викторины!"
-}
+  "English": `Hello, ${name},\n\nWelcome to QuizBot! I'm here to help you create and organize quizzes effortlessly. Just save your questions, and let's turn them into interactive quizzes!`,
+  "Hindi": "नमस्ते, ${name},\n\nQuizBot में आपका स्वागत है! मैं आपकी मदद करने के लिए यहां हूं ताकि आप आसानी से क्विज बना और आयोजित कर सकें। बस अपने प्रश्नों को सहेजें, और आइए उन्हें इंटरएक्टिव क्विज़ में बदलें!",
+  "Chinese": "你好, ${name},\n\n欢迎来到 QuizBot！我在这里帮助您轻松创建和组织测验。只需保存您的问题，让我们将它们变成互动测验！",
+  "Russian": "Здравствуйте, ${name},\n\nДобро пожаловать в QuizBot! Я здесь, чтобы помочь вам легко создавать и организовывать викторины. Просто сохраните свои вопросы, и давайте превратим их в интерактивные викторины!"
+};
+
 
 // ---------------- Tools Text ---------------- //
 
 const TOOLS_TEXT = {
-"English": "",
-"Hindi" : "",
-"Chinese": "",
-"Russian": ""
-}
+  "English": "",
+  "Hindi": "",
+  "Chinese": "",
+  "Russian": ""
+};
+
 
 // ---------------- About Text ---------------- //
 
 const ABOUT_TEXT = {
-"English": "",
-"Hindi" : "",
-"Chinese": "",
-"Russian": ""
-}
+  "English": "",
+  "Hindi": "",
+  "Chinese": "",
+  "Russian": ""
+};
 
 
-// ---------------- Questions ---------------- //
+// ---------------- Export All ---------------- //
 
- 
+module.exports = {
+  START_TEXT,
+  TOOLS_TEXT,
+  ABOUT_TEXT
+};
+
+
+
+
+
