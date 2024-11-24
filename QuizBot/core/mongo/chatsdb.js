b7fc3170c01f155fc9afd9b04d3eaaed1b345e3b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-24 23:51:29 +0530

    Update chatsdb.js

--- QuizBot/core/mongo/chatsdb.js
+++ QuizBot/core/mongo/chatsdb.js
@@ -6,17 +6,17 @@ const client = new MongoClient(MONGO_DB);
 const db = client.db("QuizBot");
 const chats = db.collection("chats");
 
-async function check_if_chats_exists(chatid) {
+async function check_if_chat_exists(chatid) {
     const chat = await chats.findOne({ "chat_id": chatid });
     return chat !== null; // Returns true if chat exists, false otherwise
 }
 
-async function add_chat(chatid, name) {
+async function add_chat(chatid, title) {
     const if_exists = await check_if_chat_exists(chatid);
     if (!if_exists) {
         await chats.insertOne({
             "chat_id": chatid,
-            "name": name
+            "title": title
         });
         console.log("Chat added successfully!");
     } else {
@@ -27,23 +27,24 @@ async function add_chat(chatid, name) {
 async function find_chat(chatid) {
     const chat = await chats.findOne({ "chat_id": chatid });
     if (chat) {
-        console.log("Chat found:", user);
+        console.log("Chat found:", chat);
         return chat;
     } else {
-        console.log("No Chat found with the given user_id.");
+        console.log("No chat found with the given chat_id.");
         return null;
     }
 }
 
 async function get_total_chats() {
     const total_chats = await chats.countDocuments({});
-    console.log("Total chats in the database:", total_users);
+    console.log("Total chats in the database:", total_chats);
     return total_chats;
 }
 
 module.exports = {
     add_chat,
-    get_total_chats
+    get_total_chats,
+    find_chat
 };
 
 
