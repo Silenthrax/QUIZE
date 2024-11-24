161255f81a3e7f7bdbaa4ad6a4af869813a73b92
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-24 17:47:35 +0530

    Update chatsdb.js

--- QuizBot/core/mongo/chatsdb.js
+++ QuizBot/core/mongo/chatsdb.js
@@ -1,9 +1,42 @@
-const {MongoClient} = require("mongodb")
-const { configs } = require("../config")
+const { MongoClient } = require("mongodb");
+const { configs } = require("../config");
 
-const client = new MongoClient(configs.MONGO_DB)
+const client = new MongoClient(configs.MONGO_DB);
+
+db = client.db("QuizBot");
+chats = db.collection("chats");
 
 
-const db = client.db("QuizBot")
-const users = db.collection("chats")
+
+async function add_chats(chat) {
+    try {
+        const result = await chats.insertOne(chat);
+        console.log("Chat added successfully:", result.insertedId);
+        return result.insertedId;
+    } catch (error) {
+        console.error("Error adding chat:", error);
+        throw error;
+    }
+}
+
+
+async function get_all_chats() {
+    try {
+        const allChats = await chats.find({}).toArray();
+        console.log("Chats retrieved successfully allChats");
+        return allChats;
+    } catch (error) {
+        console.error("Error retrieving chats:", error);
+        throw error;
+    }
+}
+
+
+module.exports = {
+    add_chats,
+    get_all_chats
+};
+
+
+
 
