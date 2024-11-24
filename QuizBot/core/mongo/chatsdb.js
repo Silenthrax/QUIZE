dd015128376573f457a5a6de79aa769f6da4f68b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-24 23:45:59 +0530

    Update chatsdb.js

--- QuizBot/core/mongo/chatsdb.js
+++ QuizBot/core/mongo/chatsdb.js
@@ -6,56 +6,44 @@ const client = new MongoClient(MONGO_DB);
 const db = client.db("QuizBot");
 const chats = db.collection("chats");
 
+async function check_if_chats_exists(chatid) {
+    const chat = await users.findOne({ "user_id": userid });
+    return chat !== null; // Returns true if user exists, false otherwise
+}
 
-
-async function add_chat(chat) {
-    try {
-        const existingChat = await chats.findOne({ _id: chat._id });
-        if (existingChat) {
-            console.log("Chat already exists. Skipping insertion.");
-            return null;
-        }
-
-        const result = await chats.insertOne(chat);
-        console.log("Chat added successfully:", result.insertedId);
-        return result.insertedId;
-    } catch (error) {
-        console.error("Error adding chat:", error);
-        throw error;
+async function add_chat(chatid, name) {
+    const if_exists = await check_if_chat_exists(chatid);
+    if (!if_exists) {
+        await chats.insertOne({
+            "chat_id": chatid,
+            "name": name
+        });
+        console.log("Chat added successfully!");
+    } else {
+        console.log("Chat already exists. Skipping insertion.");
     }
 }
 
-async function get_all_chats() {
-    try {
-        const allChats = await chats.find({}).toArray();
-        console.log("Chats retrieved successfully:", allChats.length);
-        return allChats;
-    } catch (error) {
-        console.error("Error retrieving chats:", error);
-        throw error;
+async function find_chat(chatid) {
+    const chat = await chats.findOne({ "chat_id": chatid });
+    if (chat) {
+        console.log("Chat found:", user);
+        return chat;
+    } else {
+        console.log("No Chat found with the given user_id.");
+        return null;
     }
 }
 
-async function remove_chat(chatId) {
-    try {
-        const result = await chats.deleteOne({ _id: chatId });
-        if (result.deletedCount === 1) {
-            console.log("Chat removed successfully");
-            return true;
-        } else {
-            console.log("No chat found with the specified ID");
-            return false;
-        }
-    } catch (error) {
-        console.error("Error removing chat:", error);
-        throw error;
-    }
+async function get_total_chats() {
+    const total_chats = await chats.countDocuments({});
+    console.log("Total chats in the database:", total_users);
+    return total_chats;
 }
 
 module.exports = {
     add_chat,
-    get_all_chats,
-    remove_chat,
+    get_total_chats
 };
 
 
