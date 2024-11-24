86939e48779e193f03ae2d66ba902f6cb39186f2
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-24 17:38:33 +0530

    Update chatsdb.js

--- QuizBot/core/mongo/chatsdb.js
+++ QuizBot/core/mongo/chatsdb.js
@@ -1,1 +1,9 @@
+const {MongoClient} = require("mongodb")
+const { configs } = require("../config")
+
+const client = new MongoClient(configs.MONGO_DB)
+
+
+const db = client.db("QuizBot")
+const users = db.collection("chats")
 
