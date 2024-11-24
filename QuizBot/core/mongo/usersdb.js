945ce0cbd658616c552576ea0667e4eb6344f711
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-24 23:24:32 +0530

    Update usersdb.js

--- QuizBot/core/mongo/usersdb.js
+++ QuizBot/core/mongo/usersdb.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const { MONGO_DB } = require("../../../config");
 
 const client = new MongoClient(MONGO_DB);
@@ -8,13 +8,23 @@ const users = db.collection("users");
 
 async function add_user(user) {
     try {
+        if (typeof user._id !== "string" && typeof user._id !== "number") {
+            console.error("Invalid user._id format. Must be a string or number.");
+            return null;
+        }
+
         const existingUser = await users.findOne({ _id: user._id });
         if (existingUser) {
             console.log("User already exists. Skipping insertion.");
             return null;
         }
 
-        const result = await users.insertOne(user);
+        const userWithId = {
+            _id: user._id.toString(),
+            ...user,
+        };
+
+        const result = await users.insertOne(userWithId);
         console.log("User added successfully:", result.insertedId);
         return result.insertedId;
     } catch (error) {
@@ -55,6 +65,5 @@ module.exports = {
     get_all_users,
     remove_user,
 };
-
 
 
