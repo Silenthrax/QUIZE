cd01c26e7a74f78318c91ccf5b8c26feecfb2a81
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-24 18:29:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const configs = {
   BOT_TOKEN: "7664638373:AAH7T6waS_thYfPjUEXBpPCNFAka_fxoqZo",
-  OWNER_ID: [6107581019]
+  OWNER_ID: [6107581019],
   MONGO_DB: "mongodb+srv://QuizBot:QuizBot@cluster0.slbm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 };
 
