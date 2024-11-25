b2b9be9d9ad1d5bc7a98ec68b02ec37f02c3b7cc
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-25 20:24:52 +0530

    Create stats.js

---
+++ QuizBot/plugins/stats.js
@@ -0,0 +1,36 @@
+const bot = require("../index");
+const { get_total_users } = require("../core/mongo/usersdb");
+const { get_total_chats } = require("../core/mongo/chatsdb");
+
+const Owner = 6107581019;
+
+
+bot.command("stats", async (ctx) => {
+    const users = await get_total_users();
+    const chats = await get_total_chats();
+
+    try {
+        if (ctx.message.from.id === Owner) {
+            // Fetch the bot's name dynamically
+            const botInfo = await bot.telegram.getMe();
+            const botName = botInfo.first_name;
+
+            await ctx.replyWithPhoto(
+                "https://graph.org//file/1e2e321668b57ba61d954.jpg",
+                {
+                    caption: `<b>${botName} System Stats</b>\n\n` +
+                        `Total Users: <code>${users}</code>\n` +
+                        `Total Chats: <code>${chats}</code>`,
+                    parse_mode: "HTML"
+                }
+            );
+        } else {
+            ctx.reply("This is an owner-only command.");
+        }
+    } catch (error) {
+        console.error(error);
+        ctx.reply("An error occurred while fetching the stats. Please try again later.");
+    }
+});
+
+
