18cc1adbdad6bc700476a60afb354df8ffc459a7
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-11-27 20:16:21 +0530

    Update myquiz.js

--- QuizBot/plugins/myquiz.js
+++ QuizBot/plugins/myquiz.js
@@ -1,15 +1,16 @@
 const bot = require("../index");
-const { getQuiz, getAllQuizNames } = require("../core/mongo/quizesdb");
+const { getQuiz, getAllQuizNames, removeAllQuizzes } = require("../core/mongo/quizesdb");
 
 
 // ------------------ Buttons ------------------ //
 const removeAllMarkup = {
   inline_keyboard: [
     [
-      { text: "Remove All Quizzes", callback_data: "about_" } 
-    ]]};
+      { text: "Remove All Quizzes", callback_data: "remove_all_quizzes" }
+    ]
+  ]
+};
 
-        
 // ---------------- My Quiz Command ---------------- //
 bot.command("myquiz", async (ctx) => {
   const user_id = ctx.message.from.id;
@@ -28,17 +29,19 @@ bot.command("myquiz", async (ctx) => {
   });
 
   await ctx.reply(NameText, {
-    reply_markup: removeallmarkup});
+    reply_markup: removeAllMarkup 
+  });
 });
 
 
 // ------------- Actions -------------- //
 bot.action('remove_all_quizzes', async (ctx) => {
   const user_id = ctx.from.id;
-  await removeAllQuizzes(user_id);
+  await removeAllQuizzes(user_id);  
   await ctx.answerCallbackQuery('All quizzes have been removed.');
   await ctx.editMessageText('All quizzes have been removed successfully!');
 });
+
 
 
 // ---------------- Poll Function --------------- //
